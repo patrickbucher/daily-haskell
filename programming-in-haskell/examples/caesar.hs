c47ec313f164582f7956bfd788d64f9f3fe3dd17
Patrick Bucher <patrick.bucher@mailbox.org>
2025-08-15 11:24:55 +0200

    caesar cipher: 1/2

---
+++ programming-in-haskell/examples/caesar.hs
@@ -0,0 +1,62 @@
+import Data.Char
+
+let2int :: Char -> Int
+let2int c = ord c - ord 'a'
+
+int2let :: Int -> Char
+int2let n = chr (ord 'a' + n)
+
+shift :: Int -> Char -> Char
+shift n c
+  | isLower c = int2let ((let2int c + n) `mod` 26)
+  | otherwise = c
+
+encode :: Int -> String -> String
+encode n xs = [shift n x | x <- xs]
+
+decode :: Int -> String -> String
+decode n = encode (-n)
+
+table :: [Float]
+table =
+  [ 8.1
+  , 1.5
+  , 2.8
+  , 4.2
+  , 12.7
+  , 2.2
+  , 2.0
+  , 6.1
+  , 7.0
+  , 0.2
+  , 0.8
+  , 4.0
+  , 2.4
+  , 6.7
+  , 7.5
+  , 1.9
+  , 0.1
+  , 6.0
+  , 6.3
+  , 9.0
+  , 2.8
+  , 1.0
+  , 2.4
+  , 0.2
+  , 2.0
+  , 0.1
+  ]
+
+percent :: Int -> Int -> Float
+percent n m = (fromIntegral n / fromIntegral m) * 100
+
+lowers :: [Char] -> Int
+lowers xs = sum [1 | x <- xs, isLower x]
+
+count :: Eq a => a -> [a] -> Int
+count x xs = sum [1 | x' <- xs, x' == x]
+
+freqs :: String -> [Float]
+freqs xs = [percent (count x xs) n | x <- ['a' .. 'z']]
+  where
+    n = lowers xs
