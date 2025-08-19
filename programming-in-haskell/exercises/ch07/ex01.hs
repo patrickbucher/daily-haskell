32b79b152fa587579a530407b53c91730700233d
Patrick Bucher <patrick.bucher@mailbox.org>
2025-08-19 06:05:25 +0200

    chapter 7: exercise 1

---
+++ programming-in-haskell/exercises/ch07/ex01.hs
@@ -0,0 +1,11 @@
+func :: (a -> Bool) -> (a -> b) -> [a] -> [b]
+func p f xs = [f x | x <- xs, p x]
+
+-- > func (\x -> x `mod` 2 == 0) (\x -> x * 2) [1..10]
+-- [4,8,12,16,20]
+
+func' :: (a -> Bool) -> (a -> b) -> [a] -> [b]
+func' p f = map f . filter p
+
+-- > func' (\x -> x `mod` 2 == 0) (\x -> x * 2) [1..10]
+-- [4,8,12,16,20]
