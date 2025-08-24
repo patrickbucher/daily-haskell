05c540c9c4315feff20c44730601fbf25b8f00a2
Patrick Bucher <patrick.bucher@mailbox.org>
2025-08-24 09:08:46 +0200

    chapter 8: exercise 8

---
+++ programming-in-haskell/exercises/ch08/ex08.hs
@@ -0,0 +1,65 @@
+type Assoc k v = [(k, v)]
+
+type Subst = Assoc Char Bool
+
+type Bit = Int
+
+find :: Eq a => a -> Assoc a b -> b
+find k t = head [v | (k', v) <- t, k == k']
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+
+data Prop
+  = Const Bool
+  | Var Char
+  | Not Prop
+  | And Prop Prop
+  | Or Prop Prop
+  | Imply Prop Prop
+  | Eq Prop Prop
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b) = b
+eval s (Var x) = find x s
+eval s (Not p) = not (eval s p)
+eval s (And p q) = eval s p && eval s q
+eval s (Or p q) = eval s p || eval s q
+eval s (Imply p q) = eval s p <= eval s q
+eval s (Eq p q) = eval s p == eval s q
+
+vars :: Prop -> [Char]
+vars (Const _) = []
+vars (Var x) = [x]
+vars (Not p) = vars p
+vars (And p q) = vars p ++ vars q
+vars (Or p q) = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+vars (Eq p q) = vars p ++ vars q
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False :) bss ++ map (True :) bss
+  where
+    bss = bools (n - 1)
+
+substs :: Prop -> [Subst]
+substs p = map (zip vs) (bools (length vs))
+  where
+    vs = rmdups (vars p)
+
+-- > isTaut (Or (And (Var 'a') (Not (Var 'b'))) (And (Not (Var 'a')) (Var 'b')))
+-- False
+-- > isTaut (Or (Or (Var 'a') (Var 'b')) (Or (Not (Var 'a')) (Var 'b')))
+-- True
+-- > isTaut (Eq (Not (And (Var 'a') (Var 'b'))) (Or (Not (Var 'a')) (Not (Var 'b'))))
+-- True
+-- > isTaut (Eq (Not (And (Var 'a') (Var 'b'))) (And (Not (Var 'a')) (Not (Var 'b'))))
+-- False
+isTaut :: Prop -> Bool
+isTaut p = and [eval s p | s <- substs p]
