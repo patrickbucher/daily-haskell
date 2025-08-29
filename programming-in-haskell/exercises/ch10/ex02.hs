e5d84b60a0731447651a2b0d5c1a9c937e0a2cad
Patrick Bucher <patrick.bucher@mailbox.org>
2025-08-29 07:00:40 +0200

    chapter 10: exercise 2

---
+++ programming-in-haskell/exercises/ch10/ex02.hs
@@ -0,0 +1,10 @@
+type Board = [Int]
+
+putRow :: Int -> Int -> IO ()
+putRow row num = do
+  putStr (show row)
+  putStr ": "
+  putStrLn (concat (replicate num "* "))
+
+putBoard :: Board -> IO ()
+putBoard b = sequence_ [putRow r n | (r, n) <- zip [1 ..] b]
